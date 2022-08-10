64c255f17bd67aa0e9db2539a8127c130927fe30
quantum-corn <akashkhetan044@gamil.com>
2022-08-10 20:57:21 +0530

    better

--- scripts/theme.js
+++ scripts/theme.js
@@ -2,7 +2,7 @@ var theme_icon = document.getElementById("theme_icon");
 
 function change() {
     var current = theme_icon.getAttribute("name");
-    if current =="moon_outline" {
+    if (current =="moon_outline") {
         document.getElementById("theme_style").setAttribute("href", "./styles/light_theme.css");
         theme_icon.setAttribute("name", "sunny_outline");
     }

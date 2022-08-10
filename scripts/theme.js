ddb7f7a4a4280111e2a6e297b4810cdada7f9bdc
quantum-corn <akashkhetan044@gamil.com>
2022-08-10 20:03:22 +0530

    theme button working prolly

---
+++ scripts/theme.js
@@ -0,0 +1,15 @@
+var theme_icon = document.getElementById("theme_icon");
+
+function change() {
+    var current = theme_icon.getAttribute("name");
+    if current =="moon_outline" {
+        document.getElementById("theme_style").setAttribute("href", "./styles/light_theme.css");
+        theme_icon.setAttribute("name", "sunny_outline");
+    }
+    else {
+        document.getElementById("theme_style").setAttribute("href", "./styles/dark_theme.css");
+        theme_icon.setAttribute("name", "moon_outline");
+    }
+}
+
+theme_icon.addEventListener("click", change());

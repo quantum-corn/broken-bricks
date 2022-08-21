74d08253d7f2489c03ab79660bff8136aff9c749
quantum-corn <akashkhetan044@gamil.com>
2022-08-21 17:24:47 +0530

    streamlining, optimization, mobile-ready framework

---
+++ scripts/setup.js
@@ -0,0 +1,10 @@
+function setup() {
+    var buttons = document.querySelectorAll("nav li");
+    var contents = document.querySelectorAll("#content_box>main");
+
+    buttons[0].id = "current_tab";
+    contents[0].id = "current_cont";
+}
+
+
+setup();

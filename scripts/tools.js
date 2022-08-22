276fc7c1187fa15d0b9e5199afb40f9fbfce832a
quantum-corn <akashkhetan044@gamil.com>
2022-08-22 16:06:37 +0530

    optimization, more structure

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,14 +1,15 @@
 function theme() {
     function change() {
         var current = theme_icon.getAttribute("name");
+        var href=document.getElementById("theme_style").getAttribute("href");
         
         if (current == "moon") {
-            document.getElementById("theme_style").setAttribute("href", "./styles/dark_theme.css");
+            document.getElementById("theme_style").setAttribute("href", href.replace("light", "dark"));
             theme_icon.setAttribute("name", "sunny");
         }
         
         else {
-            document.getElementById("theme_style").setAttribute("href", "./styles/light_theme.css");
+            document.getElementById("theme_style").setAttribute("href", href.replace("dark", "light"));
             theme_icon.setAttribute("name", "moon");
         }
     }
@@ -22,7 +23,7 @@ function tab(){
         buttons[current].id='';
         contents[current].id='';
         this.id="current_tab";
-        for (i=0; i<buttons.length; i++) {
+        for (i=0; i<buttons.length-1; i++) {
             if (buttons[i]==this) {
                 contents[i].id="current_cont";
                 current=i;
@@ -34,7 +35,7 @@ function tab(){
     var contents=document.querySelectorAll("#content_box>main");
     var current=0;
     
-    for (i=0; i<buttons.length; i++) {
+    for (i=0; i<buttons.length-1; i++) {
         if (buttons[i].id=="current_tab") {
             current=i;
         }

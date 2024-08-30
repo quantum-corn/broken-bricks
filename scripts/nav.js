43de0d63e2e421da821bc458a2a8ef97419ff137
quantum-corn <akashkhetan044@gmail.com>
2024-08-30 18:43:23 +0530

    Develop the hover effect to show navigation label
    Also change the hover effect shape to a circle.

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,11 +4,25 @@ const BGCOLOR="#342519";
 
 const hoverEffect = function(e, state) {
     setBg(e, state);
+    toggleLabel(e, state);
 }
 
 const setBg = function(e, state) {
-    console.dir(e.target);
     e.target.style.backgroundColor=state?HLTCOLOR:BGCOLOR;
+}
+
+const toggleLabel = function(e, state) {
+    let item=e.target.parentElement;
+    if(state) {
+        let label=document.createElement("span");
+        label.textContent=item.className;
+        label.className="label"
+        item.appendChild(label);
+    }
+    else {
+        let label=item.querySelector(".label");
+        item.removeChild(label);
+    }
 }
 
 const navBtn = document.querySelectorAll(".nav li ion-icon");
